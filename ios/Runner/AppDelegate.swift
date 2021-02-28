2d24a0ebf56f1d47844cd5a245f5cddb080f9907
nmp99 <ph.nam990@gmail.com>
2021-02-28 23:02:18 +0700

    new commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,13 +2,21 @@ import UIKit
 import Flutter
 import GoogleMaps
 import UserNotifications
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+override init() {
+    super.init()
+    FirebaseApp.configure()
+  }
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
     GMSServices.provideAPIKey("AIzaSyADQl79AootGyhKCW8MQ8xxz561gPFu0rA")
     GeneratedPluginRegistrant.register(with: self)
     if #available(iOS 10.0, *) {
